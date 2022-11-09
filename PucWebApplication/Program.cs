0bcd3b41d8f48b344f498c42053059d848443b4c
Guilherme Henrique Monteiro Cal <103080396+TioTaylor@users.noreply.github.com>
2022-11-08 21:12:50 -0300

    Branch Desenvolvimento 1

---
+++ PucWebApplication/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using PucWebApplication.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//alterar o server quando for subir para a núvem.
+builder.Services.AddDbContext<Contexto>
+                (options => options.UseMySql(
+                    "Server=MYSQL8001.site4now.net;Database=db_a8ef03_faxina;Uid=a8ef03_faxina;Pwd=@Minhafaxina1",
+                    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql")));
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options => {
+                options.AccessDeniedPath = "/Usuarios/AccessDenied/";
+                options.LoginPath = "/Usuarios/Login/";
+
+            });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCookiePolicy();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
