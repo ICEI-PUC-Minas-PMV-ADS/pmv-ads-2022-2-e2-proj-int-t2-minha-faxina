d67463d1118a6fcf8f7bc1a4eb93e65e5c8f58b5
PedrowiskDev <pedro.soares.1373928@sga.pucminas.br>
2022-11-14 21:20:21 -0300

    Pagina Pesquisa

---
+++ PucWebApplication/Controllers/PesquisaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PucWebApplication.Controllers
+{
+    public class PesquisaController : Controller
+    {
+        public IActionResult Pesquisa()
+        {
+            return View();
+        }
+    }
+}
