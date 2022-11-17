34d85fd0242c6256afee1293be5cdb26de7fdaf3
Rafael Souza <rafael.souza54@hotmail.com>
2022-11-16 21:30:58 -0300

    pagina perfil

---
+++ PucWebApplication/Views/Usuarios/MeuPerfil.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PucWebApplication.Views.Usuarios
+{
+    public class MeuPerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
