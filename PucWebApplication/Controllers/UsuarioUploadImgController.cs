588269a00cbe4666061bf904cafa6e3e00e2a549
Rafael Souza <rafael.souza54@hotmail.com>
2022-11-26 20:46:46 -0300

    carregamento de imagem para o banco

---
+++ PucWebApplication/Controllers/UsuarioUploadImgController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PucWebApplication.Data;
+using PucWebApplication.Models;
+
+namespace PucWebApplication.Controllers
+{
+    public class UsuarioUploadImgController : Controller
+    {
+        private readonly Contexto _context;
+
+        public UsuarioUploadImgController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: UsuarioUploadImg
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Usuario.ToListAsync());
+        }
+
+        // GET: UsuarioUploadImg/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // GET: UsuarioUploadImg/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioUploadImg/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Email,cpf,Idade,cep,Rua,Numero,Complemento,Bairro,Cidade,Senha,Perfil,tel,EmpPhotoPath,EmpFileName")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: UsuarioUploadImg/Edit/5
+
+        public ActionResult Edit(int id) {
+            
+
+            return View();
+        }
+
+        private dynamic RetornaDados() {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: UsuarioUploadImg/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email,cpf,Idade,cep,Rua,Numero,Complemento,Bairro,Cidade,Senha,Perfil,tel,EmpPhotoPath,EmpFileName")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // GET: UsuarioUploadImg/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Usuario == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // POST: UsuarioUploadImg/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Usuario == null)
+            {
+                return Problem("Entity set 'Contexto.Usuario'  is null.");
+            }
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuario.Remove(usuario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+          return _context.Usuario.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PucWebApplication/Migrations/20221126144352_M05.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PucWebApplication.Migrations
+{
+    public partial class M05 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmpFileName",
+                table: "Usuarios",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmpPhotoPath",
+                table: "Usuarios",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmpFileName",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "EmpPhotoPath",
+                table: "Usuarios");
+        }
+    }
+}
