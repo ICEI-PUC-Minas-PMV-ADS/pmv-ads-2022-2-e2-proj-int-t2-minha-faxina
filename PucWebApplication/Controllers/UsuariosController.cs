4074ac7843426114d5c4224c046e23d19ee32359
TioTaylor <1083465@sga.pucminas.br>
2022-11-27 20:37:32 -0300

    Update Upload de Imagens

--- PucWebApplication/Controllers/UsuariosController.cs
+++ PucWebApplication/Controllers/UsuariosController.cs
@@ -174,7 +174,7 @@ namespace PucWebApplication.Controllers
                 usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(login));
+                return Redirect("/Home");
             }
             return View(usuario);
         }
